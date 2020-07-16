acdbc12cc849939b512300e606693ea13460276f
gustavo.silva <gustavo.silva@codificar.com.br>
2020-07-16 20:35:43 -0300

    first commit

---
+++ learning/pages/users/_id/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h2>User ID: {{$route.params.id}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    validate({params}){
+        return /^\d+$/.test(params.id)
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ learning/pages/users/index.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <h1>Users Page</h1>
+
+  <input type="text" v-model="id">
+  <button @click="loadUser">Load User</button>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            id: ''
+        }
+    },
+    methods: {
+        loadUser(){
+            //this.$router.push Joga para a rota do parametro
+            this.$router.push(`/users/${this.id}`)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
