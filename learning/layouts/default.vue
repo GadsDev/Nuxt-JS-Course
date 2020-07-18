d2fe48d54470c7fed3f7c0891af9bb3f79dc3f1b
gustavo.silva <gustavo.silva@codificar.com.br>
2020-07-17 22:39:13 -0300

    Nav Bar

--- learning/layouts/default.vue
+++ learning/layouts/default.vue
@@ -1,5 +1,20 @@
-<template>
+<template> 
   <div>
+    <Nav />
     <Nuxt />
   </div>
 </template>
+
+<script>
+  import Nav from '@/components/Nav'
+export default {
+  components: {
+    Nav
+  }
+
+}
+</script>
+
+<style>
+
+</style>
